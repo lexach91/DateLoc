dabce0a3d315c245495df40e437e6e07b879d334
Steve <la14b@yahoo.co.uk>
2022-02-25 13:27:04 +0000

    added migrations

--- chat/models.py
+++ chat/models.py
@@ -12,7 +12,7 @@ class Conversations(models.Model):
 class Messages(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender", unique=False)
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver", unique=False)
-    conversation = models.ForeignKey(Conversations, related_name="conversations")
+    conversation = models.ForeignKey(Conversations, on_delete=models.CASCADE, related_name="conversations")
     message_content = models.TextField(max_length=500, default='', blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False, blank=False)
