4b284e393fd6f11a3454568b141c16c472599318
Steve <la14b@yahoo.co.uk>
2022-02-25 12:27:58 +0000

    Added location_match models and admin

--- location_match/admin.py
+++ location_match/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Location_Match
 
-# Register your models here.
+
+@admin.register(Location_Match)
+class Location_MatchAdmin(admin.ModelAdmin):
+    """Filters, displays and search for django admin"""
+    list_filter = ('match', 'user_profile', 'location', 'user', )
+    list_display = ('user_profile', 'location', 'user')
+    search_fields = ['match', 'user_profile', 'location', 'user']
--- location_match/models.py
+++ location_match/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from profiles.models import UserProfile
+
+
+class Location_Match(models.Model):
+    """
+    Model to match users location
+    """
+    match = models.ManyToManyField('self')
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='match')
+    user = models.ForeignKey(User, on_delete=models.SET_NULL,
+                             null=True, blank=True, related_name='matched')
+    location = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                 null=True, blank=True, related_name='location')
+
+    def __str__(self):
+        return self.match
