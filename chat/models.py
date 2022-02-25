77d62c2aad2b44de66b1b252bba8b70d280776ca
Steve <la14b@yahoo.co.uk>
2022-02-25 13:10:45 +0000

    Add Chat app models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Conversations(models.Model):
+    participants = models.ManyToManyField(User, related_name="participants", unique=False)
+
+    def __str__(self):
+        return self.participants
+
+
+class Messages(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender", unique=False)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver", unique=False)
+    conversation = models.ForeignKey(Conversations, related_name="conversations")
+    message_content = models.TextField(max_length=500, default='', blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False, blank=False)
+
+    def __str__(self):
+        return self.sender
