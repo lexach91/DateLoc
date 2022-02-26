4a576e38c0883df86e559c80b383305aa48bc156
lexach91 <ronmctailor@protonmail.com>
2022-02-26 20:54:03 +0800

    Include static in urlpatterns

--- DateLoc/urls.py
+++ DateLoc/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('location_match/', include('location_match.urls')),
     path('profiles/', include('profiles.urls')),
     path('account/', include('allauth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
