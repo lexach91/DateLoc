cb9e809a18d7fc4e11d90bb9a5fd56fbe5e2dc7f
Steve <la14b@yahoo.co.uk>
2022-02-26 14:00:00 +0000

    Add create profile view

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name="profile"),
+    path('create-profile/', views.ProfileCreateView.as_view(), name="create-profile"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import DetailView
+from django.urls import reverse_lazy, reverse
+from .models import UserProfile
 
 
 def profile(request):
-    '''Profile page view'''
-    return render(request, "profiles/profiles.html")
+    '''Home page view'''
+    return render(request, "profiles/profile.html")
+
+
+class ProfileCreateView(CreateView):
+    '''Profile Detail view'''
+    model = UserProfile
+    template_name = 'profiles/create_profiles.html'
+    fields = ['display_name', 'email', 'bio', 'image', 'job_title', 'phone_number',
+              'town_or_city', 'county']
+    success_message = "Profile created"
+    success_url = reverse_lazy('profile')
+
+
+# class ProfileDetailView(DetailView):
+#     '''Profile Detail view'''
+#     model = UserProfile
+#     template_name = 'profiles/profile.html'
+#     fields = '__all__'
