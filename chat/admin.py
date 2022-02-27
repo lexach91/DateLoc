a97593e12f328833bb8d3826b11b622684b4951f
lexach91 <ronmctailor@protonmail.com>
2022-02-28 00:04:07 +0800

    Add ajax handler to save messages to DB

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import PrivateChat, Message
 
 # Register your models here.
+@admin.register(PrivateChat)
+class PrivateChatAdmin(admin.ModelAdmin):
+    """Filters, displays and search for django admin"""
+    list_filter = ('user1', 'user2', )
+    list_display = ('user1', 'user2')
+    search_fields = ['user1', 'user2']
+    
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    """Filters, displays and search for django admin"""
+    list_filter = ('chat', 'sender', )
+    list_display = ('chat', 'sender', 'message', 'date')
+    search_fields = ['chat', 'sender']
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
 from .models import PrivateChat, Message
 
 
@@ -6,11 +7,25 @@ def chat(request, chat_id):
     '''Chat page view'''
     if not request.user.is_authenticated:
         return render(request, "chat/permission_denied.html")
-    chat = PrivateChat.objects.get(id=chat_id)
+    chat = get_object_or_404(PrivateChat, pk=chat_id)
     if request.user.id not in [chat.user1.id, chat.user2.id]:
         return render(request, "chat/permission_denied.html")
-    messages = Message.objects.filter(chat=chat)
+    messages = chat.messages.all().order_by("date")
     return render(request, "chat/chat.html", {
         'chat_id': chat.id,
         'messages': messages,
     })
+    
+    
+def save_message(request, chat_id):
+    '''Save message to database'''
+    if request.is_ajax() and request.user.is_authenticated:
+        chat = get_object_or_404(PrivateChat, pk=chat_id)
+        if request.user.id not in [chat.user1.id, chat.user2.id]:
+            return JsonResponse({'error': 'You are not allowed to send messages to this chat'})
+        message = request.POST.get("message")
+        sender = request.user
+        if message:
+            Message.objects.create(chat=chat, sender=sender, message=message)
+            return JsonResponse({'success': True})
+        
