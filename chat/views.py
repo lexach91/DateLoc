6f2038d4620381beadf0bc8212865cd49472d5f6
lexach91 <ronmctailor@protonmail.com>
2022-02-27 23:00:27 +0800

    Rewrite view for chat to allow only certain users

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from .models import PrivateChat, Message
 
 
 def chat(request, chat_id):
     '''Chat page view'''
-    return render(request, "chat/chat.html", {
-        'chat_id': chat_id,
-    })
+    # return render(request, "chat/chat.html", {
+    #     'chat_id': chat_id,
+    # })
+    
+    if request.user.is_authenticated:
+        chat = PrivateChat.objects.get(id=chat_id)
+        if request.user.id == chat.user1.id or request.user.id == chat.user2.id:
+            messages = Message.objects.filter(chat=chat)
+            return render(request, "chat/chat.html", {
+                'chat_id': chat.id,
+                'messages': messages,
+            })
+        else:
+            return render(request, "chat/permission_denied.html")
+    else:
+        return render(request, "chat/permission_denied.html")
