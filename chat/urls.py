f929402148b522121e28819a715ec9e91b5face1
lexach91 <ronmctailor@protonmail.com>
2022-02-28 21:19:19 +0800

    Rewrite url for chat creation

--- chat/urls.py
+++ chat/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('<int:chat_id>/', views.chat, name="chat"),
     path('save_message/<int:chat_id>/', views.save_message, name="save_message"),
-    path('get_or_create_chat/<str:user2>', views.get_or_create_chat, name="get_or_create_chat"),
+    path('get_or_create_chat/<int:user2>', views.get_or_create_chat, name="get_or_create_chat"),
 ]
